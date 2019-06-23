edde4388cdc2c7e76908b78bdabbaf2410e54209
Lupo Montero <lupomontero@gmail.com>
2019-06-23 15:19:38 -0500

    wip

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,56 +1,49 @@
 # /usr/bin/env bash
 
 
-if [[ "$TRAVIS_NODE_VERSION" != "12" ]]; then
-  echo "Not Node v12.x build. Ignoring..."
+gitSetup() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+
+commitAndPushChanges() {
+  echo "Changes found! Creating new branch and commitingc changes..."
+  local branch="$1"
+  git checkout -b "${branch}"
+  git add .
+  git commit -m "chore(house-keeping): Updates date"
+  echo "Creating pull request..."
+  git remote add origin-with-token https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin-with-token "${branch}"
+}
+
+sendPR() {
+  local branch="$1"
+  curl \
+    -X POST \
+    -H "Authorization: token ${GH_TOKEN}" \
+    -d "{\"title\":\"Updates date\",\"head\":\"${branch}\",\"base\":\"master\"}" \
+    https://api.github.com/repos/${TRAVIS_REPO_SLUG}/pulls
+}
+
+
+if [[ "$TRAVIS_EVENT_TYPE" != "cron" || "$TRAVIS_NODE_VERSION" != "12" || "$TRAVIS_BRANCH" != "master" ]]; then
+  echo "Not triggered by cron. Ignoring..."
   exit 0
 fi
 
-if [[ "$TRAVIS_BRANCH" != "master" ]]; then
-  echo "Not in master. Ignoring..."
-  exit 0
-fi
 
-if [[ "$TRAVIS_EVENT_TYPE" == "cron" ]]; then
-  echo "Build triggered by Travis CI Cron"
-fi
-
-
+echo "Build triggered by Travis CI Cron"
 npm run update
-
-
 git diff-index --quiet HEAD --
 if [[ "$?" == 0 ]]; then
   echo "Already up to date."
   exit 0
 fi
 
-
-echo "Changes found!"
-echo "Creating pull request..."
 ts=$( date +%s )
 branch="update-date-${ts}"
-
-
-# git setup
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "Travis CI"
-
-
-# create branch and add commit changes
-git checkout -b "${branch}"
-git add .
-git commit -m "chore(house-keeping): Updates date"
-
-
-# Push changes and create PR
-git remote add origin-with-token https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git > /dev/null 2>&1
-git push --quiet --set-upstream origin-with-token "${branch}"
-
-
-# send PR
-curl \
-  -X POST \
-  -H "Authorization: token ${GH_TOKEN}" \
-  -d "{\"title\":\"Updates date\",\"head\":\"${branch}\",\"base\":\"master\"}" \
-  https://api.github.com/repos/${TRAVIS_REPO_SLUG}/pulls
+gitSetup
+commitAndPushChanges $branch
+sendPR $branch
