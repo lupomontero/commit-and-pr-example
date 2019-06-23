5cd38432a24c2a624ae9f0a41762908d43564f6f
Lupo Montero <lupomontero@gmail.com>
2019-06-23 14:26:32 -0500

    wip

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,7 @@
 # /usr/bin/env bash
 
+env | grep NODE
+
 if [[ "$TRAVIS_EVENT_TYPE" == "cron" ]]; then
   echo "Build triggered by Travis CI Cron"
 fi
@@ -16,12 +18,23 @@ echo "Changes found!"
 echo "Creating pull request..."
 ts=$( date +%s )
 branch="update-date-${ts}"
+
 # git setup
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
+
 # create branch and add commit changes
 git checkout -b "${branch}"
 git add .
 git commit -m "chore(house-keeping): Updates date"
+
 # Push changes and create PR
-git remote -v
+git remote add origin-with-token https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git > /dev/null 2>&1
+git push --quiet --set-upstream origin-with-token "${branch}"
+
+# send PR
+curl \
+  -X POST \
+  -H "Authorization: token ${GH_TOKEN}" \
+  -d "{\"title\":\"Updates date\",\"head\":\"${branch}\",\"base\":\"master\"}" \
+  https://api.github.com/repos/${TRAVIS_REPO_SLUG}/pulls
